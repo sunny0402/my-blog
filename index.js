6baad23e08abd5d2acc054e1fb0b21567f2ff340
sunny0402 <sunnycodes0402@gmail.com>
2022-03-09 15:09:57 -0500

    In production serve from /build and in development serve from /public.

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.use(cors());
 // if deployed serve static files from build folder
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build/")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    // res.sendStatus(err.status || 404);
+  });
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public/index.html")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  });
 }
 
 async function database_start() {
