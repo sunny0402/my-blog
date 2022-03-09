9a9556319a3d2cc7e2dcfa8342906825c6cd41f8
sunny0402 <sunnycodes0402@gmail.com>
2022-03-09 15:38:23 -0500

    Test removing links.

--- client/src/App.js
+++ client/src/App.js
@@ -38,14 +38,14 @@ class App extends Component {
 
     return (
       <div className="app">
-        <nav style={{ margin: 10 }}>
+        {/* <nav style={{ margin: 10 }}>
           <Link to="/" style={{ padding: 5 }}>
             Home
           </Link>
           <Link to="/about" style={{ padding: 5 }}>
             About
           </Link>
-        </nav>
+        </nav> */}
         <Routes>
           <Route
             path="/"
